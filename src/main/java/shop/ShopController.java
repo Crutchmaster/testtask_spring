a9cfefe7f121fbc02522b167311606566fd01d54
crutchmaster <crutchmaster@yandex.ru>
2017-05-24 17:51:17 +0700

    Add views

--- src/main/java/shop/ShopController.java
+++ src/main/java/shop/ShopController.java
@@ -3,44 +3,43 @@ package shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ShopController {
     @Autowired
 	TypesRepository typesRepo;
-	@Autowired
 	ItemsRepository itemsRepo;
+    AttrsRepository attrsRepo;
+    AttrValuesRepository attrValuesRepo;
+    BrandsRepository brandsRepo;
+    ItemAttrsRepository ItemAttrsRepo;
     @RequestMapping("/")
-    public String index() {
-		String res = "<html>";
-		for (Item i : itemsRepo.findAll()) {
-			res += "<div>"+i.toString()+"</div>";
-		}
-
-		return res += "</html>";
+    public String index(Model model) {
+		return "main";
     }
 
     @RequestMapping("/init")
     public String init() {
+        typesRepo.deleteAll();
         typesRepo.save(new Type("int"));
-        typesRepo.save(new Type("String"));
+        typesRepo.save(new Type("string"));
         typesRepo.save(new Type("bool"));
+
         return "OK";
     }
 
     @GetMapping("/view/{data}")
-    public String view(@PathVariable String data) {
-        String res = "<html>";
-        JpaRepository<?, Long> repo = typesRepo;
+    public String view(@PathVariable String data, Model model) {
+        JpaRepository<?, Long> repo = itemsRepo;
         switch (data) {
             case "types" : repo = typesRepo; break;
+            default : data = "items";
         }
-
-        for (Object obj : repo.findAll()) {
-			res += "<div>"+obj.toString()+"</div>";
-		}
-        res += "</html>";
-        return res;
+        model.addAttribute("data", data);
+        model.addAttribute("list", repo.findAll());
+        return "list";
     }
     
 }
